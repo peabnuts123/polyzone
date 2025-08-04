30c60aae353e166db127aa2db31e2957fe12f21f
peabnuts123 <peabnuts123@gmail.com>
2025-08-04 20:23:36 +1200

    Combine separate editor vitest config files into one (didn't realise this was a thing)

--- src/editor/src/web/vitest.config.ts
+++ src/editor/src/web/vitest.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+
+/**
+ * Glob pattern for any script file extension.
+ * Explanation:
+ *  ?(c|m)   "c" or "m" (optional group)
+ *  [jt]     "j" or "t"
+ *  s        "s"
+ *  ?(x)     "x" (optional group)
+ */
+export const ScriptFileExtensionGlob = `?(c|m)[jt]s?(x)`;
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
@@ -9,19 +20,69 @@ export default defineConfig({
     'test/integration/mock/assets/scripts/*',
     'test/integration/mock/assets/textures/*',
   ],
+  resolve: {
+    alias: {
+      '@polyzone/runtime/src': path.resolve(__dirname, '../../../runtime/src'),
+      '@polyzone/core/src': path.resolve(__dirname, '../../../core/src'),
+    },
+  },
   test: {
-    exclude: [
-      '**\/node_modules/**',
-      '**\/.git/**',
-      '**/*.integration.test.?(c|m)[jt]s?(x)',
-    ],
     setupFiles: [
       'test/setup/deregisterAllFileSystems.ts',
     ],
-    environment: 'happy-dom', // @TODO Probably none of this
     expect: {
       requireAssertions: true,
     },
     reporters: ['verbose'],
+    coverage: {
+      clean: true,
+      include: [
+        `src/**/*.${ScriptFileExtensionGlob}`,
+        `lib/**/*.${ScriptFileExtensionGlob}`,
+      ],
+      exclude: ['test/**/*'],
+      reportsDirectory: 'coverage/unit',
+      reporter: ['json', 'html'],
+    },
+    projects: [
+      {
+        /* Unit tests */
+        extends: true,
+        test: {
+          name: "Unit tests",
+          exclude: [
+            '**\/node_modules/**',
+            '**\/.git/**',
+            `**/*.integration.test.${ScriptFileExtensionGlob}`,
+          ],
+          environment: 'happy-dom', // @TODO Needed?
+        },
+      },
+      {
+        /* Integration tests */
+        extends: true,
+        test: {
+          name: "Integration tests",
+          testTimeout: 3000,
+          include: [
+            `**/*.integration.test.${ScriptFileExtensionGlob}`,
+          ],
+          setupFiles: [
+            'test/setup/mockTauri.ts',
+          ],
+          browser: {
+            provider: 'playwright',
+            enabled: true,
+            headless: true,
+            instances: [
+              {
+                browser: 'chromium',
+                screenshotFailures: false,
+              },
+            ],
+          },
+        },
+      },
+    ],
   },
 });
